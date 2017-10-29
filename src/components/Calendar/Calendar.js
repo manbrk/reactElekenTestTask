bd5ea65c04329c0f0229c116bca52d3775e90bfb
manbrk <manbrk@gmail.com>
2017-10-29 13:24:33 +0200

    calendar day picker added

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,14 +1,31 @@
 import React, {Component} from 'react'
+import DayPicker, {DateUtils} from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
 
 class Calendar extends Component {
+  state = {
+    from: null,
+    to: null
+  };
 
-    render() {
-        return (
-            <div>
-              <h1>Calendar component</h1>
-            </div>
-        )
-    }
+  handleDayClick = (day) => {
+    this.setState(DateUtils.addDayToRange(day, this.state))
+  };
+
+  render() {
+    const {from, to} = this.state;
+    const selectRange = from && to && `${from.toDateString()} - ${to.toDateString()}`;
+    return (
+      <div className="date-range">
+        <DayPicker
+          ref="daypicker"
+          selectedDays={day => DateUtils.isDayInRange(day, {from, to})}
+          onDayClick={this.handleDayClick}
+        />
+        {selectRange}
+      </div>
+    )
+  }
 }
 
 export default Calendar
