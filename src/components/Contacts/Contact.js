e18f17052d9834818df8b500f9a42a58235e4c5b
manbrk <manbrk@gmail.com>
2017-10-29 19:22:34 +0200

    markup fixes

--- src/components/Contacts/Contact.js
+++ src/components/Contacts/Contact.js
@@ -4,17 +4,25 @@ import './Contact.css'
 
 class Contact extends Component {
 
+  icon = null;
+
   render() {
     const {contact} = this.props;
-
-
 
     return (
       <div className="Contact">
         <div>{contact.title}</div>
-        <div><a href={contact.link}>Visit here</a></div>
+        <div><a href={contact.link}>{this.getIcon()}</a></div>
       </div>
     )
+  }
+
+  getIcon() {
+    const {contact} = this.props;
+
+    if (contact.icon === 'email') return <i className="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
+    if (contact.icon === 'linkedin') return <i className="fa fa-linkedin fa-2x" aria-hidden="true"></i>
+    if (contact.icon === 'github') return <i className="fa fa-github fa-2x" aria-hidden="true"></i>
   }
 }
 
